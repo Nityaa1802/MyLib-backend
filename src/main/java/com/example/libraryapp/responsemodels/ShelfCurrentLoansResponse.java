333bc3488c7c5ce3cb2f7f2cf31d9fe93e11516b
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-04-23 18:24:45 +0530

    created AIs for managing the shelf and history of the user

---
+++ src/main/java/com/example/libraryapp/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,19 @@
+package com.example.libraryapp.responsemodels;
+
+
+import com.example.libraryapp.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    private Book book;
+
+    private int daysLeft;
+
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+}
